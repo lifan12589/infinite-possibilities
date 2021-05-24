c50c6d0212f9f27c142de34c820c68f92265ef60
lifan12589 <lifan12589@126.com>
2021-05-24 16:04:36 +0800

    delete ac-new

--- ac-new/src/main/java/com/infinitePossibilities/utils/Listener/new1/RobotListener.java
+++
@@ -1,6 +0,0 @@
-package com.infinitePossibilities.utils.Listener.new1;
-
-public interface RobotListener {
-	public void working(Even even);
-    public void dancing(Even even);
-}
--- ac-new/src/main/java/com/infinitePossibilities/youHiTest/PayTest/Strategy.java
+++
@@ -1,9 +0,0 @@
-package com.infinitePossibilities.youHiTest.PayTest;
-
-import java.math.BigDecimal;
-
-public interface Strategy {
-
-	BigDecimal  calRecharge(Integer channelId, Integer goodsId, String zhekouId);
-	
-}
